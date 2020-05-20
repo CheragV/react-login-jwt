46041f050993079669131d9223bc732eb54d9635
Cherag Verma <cherag@ixigo.com>
2020-05-20 15:18:29 +0530

    Updates

---
+++ precache-manifest.02d1c37c899bbef367c20131b2da33e7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f3b8ed5abb432013532b30eb6aaef41",
+    "url": "/secure-login/index.html"
+  },
+  {
+    "revision": "1a531466278d20a44ef1",
+    "url": "/secure-login/static/css/main.f0415d40.chunk.css"
+  },
+  {
+    "revision": "eaff10ab8f539d34553e",
+    "url": "/secure-login/static/js/2.1b733190.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/secure-login/static/js/2.1b733190.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a531466278d20a44ef1",
+    "url": "/secure-login/static/js/main.5cc0d757.chunk.js"
+  },
+  {
+    "revision": "c69dd445868af4200c65",
+    "url": "/secure-login/static/js/runtime-main.86f4a3a5.js"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/secure-login/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/secure-login/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/secure-login/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/secure-login/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/secure-login/static/media/roboto-v20-latin-regular.8681f434.svg"
+  }
+]);
--- src/components/index.js
+++
@@ -1,7 +0,0 @@
-import Create from './Create';
-import Login from './Login';
-
-export {
-    Create,
-    Login
- }
